dde5d33339562b7c5bdd5832303113ca39a79d97
anaspolygon <al.anas@polygontech.xyz>
2025-09-17 13:40:35 +0600

    update create user dto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,7 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
   id: number;
+
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty()
+  @MinLength(3, {
+    message: 'Name is too short. Minimum length is $constraint1 characters',
+  })
   name: string;
-  geder: string;
+
+  @IsString()
+  @IsOptional()
+  gender?: string;
+
+  @IsEmail()
   email: string;
   isMarried: boolean;
 }
