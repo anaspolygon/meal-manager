36ecff0fd27694dd19796e6b25e6ba73073d96e5
anaspolygon <al.anas@polygontech.xyz>
2025-09-20 01:23:54 +0600

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -12,6 +13,15 @@ async function bootstrap() {
       transform: true, // automatically transform payloads to be objects typed according to their DTO classes
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Meal Manager')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
