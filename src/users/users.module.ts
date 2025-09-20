75def995da49ac89044e27eb6e1188720c57cc29
anaspolygon <al.anas@polygontech.xyz>
2025-09-21 01:20:13 +0600

    update create user method

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { Users } from './users.entity';
+import { Profile } from 'src/profile/profile.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users,Profile])],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { Users } from './users.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { Profile } from 'src/profile/profile.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private readonly usersRepository: Repository<Users>,
+    @InjectRepository(Profile)
+    private readonly profileRepository: Repository<Profile>,
   ) {}
 
   public getAllUsers() {
@@ -18,14 +21,16 @@ export class UsersService {
   }
 
   public async createUser(userDto: CreateUserDto) {
-    console.log(userDto);
     const user = await this.usersRepository.findOne({
       where: { email: userDto.email },
     });
     if (user) {
       return 'User already exists';
     }
+    let profile = this.profileRepository.create(userDto.profile ?? {});
+    await this.profileRepository.save(profile);
     let newUser = this.usersRepository.create(userDto);
+    newUser.profile = profile;
     newUser = await this.usersRepository.save(newUser);
     return newUser;
   }
