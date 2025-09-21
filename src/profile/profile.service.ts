1b53459ea3ab55af81491b4dc57f5312d4a60d6c
anaspolygon <al.anas@polygontech.xyz>
2025-09-21 13:59:59 +0600

    update profile

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,16 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from './profile.entity';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectRepository(Profile)
+    private readonly porfileRepository: Repository<Profile>,
+  ) {}
+
+  public getAllProfiles() {
+    return this.porfileRepository.find();
+  }
+}
