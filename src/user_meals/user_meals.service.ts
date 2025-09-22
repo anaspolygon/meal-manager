b0d6d205aea0278e35777bc02c6bd8b480bab0b5
anaspolygon <al.anas@polygontech.xyz>
2025-09-23 02:13:16 +0600

    update

--- src/user_meals/user_meals.service.ts
+++ src/user_meals/user_meals.service.ts
@@ -12,7 +12,8 @@ export class UserMealsService {
   ) {}
 
   public async createMeal(userMeal: CreateUserMealsDto) {
-    const meal = this.userMealsRepository.create(userMeal);
+    console.log(userMeal);
+    const meal = this.userMealsRepository.create({...userMeal,user:{id:userMeal.userId}});
     return await this.userMealsRepository.save(meal);
   }
 }
