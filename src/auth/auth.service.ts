f477d2ce739e9ff79e54f6fd2706304ff7b034bd
anaspolygon <al.anas@polygontech.xyz>
2025-10-07 17:36:54 +0600

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,26 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
+import { HashingProvider } from './provider/hashing.provider';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UsersService))
     private readonly usersService: UsersService,
+    private readonly hashingProvider: HashingProvider,
   ) {}
 
   public async login(loginDto: LoginDto) {
     const user = await this.usersService.findUserByEmail(loginDto.email);
-    return user;
+    let isEqual = false;
+    if (user) {
+      isEqual = await this.hashingProvider.comparePassword(
+        loginDto.password,
+        user?.password,
+      );
+    }
+    return isEqual;
   }
   public async signup(createUserDto: CreateUserDto) {
     return await this.usersService.createUser(createUserDto);
